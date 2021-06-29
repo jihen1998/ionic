3262dece1c8b1cc6c2fdf55bf8a44ab7915cd4fc
jihen1998 <57370028+jihen1998@users.noreply.github.com>
2021-06-29 03:51:27 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import {React,useState} from "react";
+import { Route } from 'react-router-dom';
+import GoalList from "./components/test";
+import NewGoal from "./components/newGoal";
+import '@ionic/react/css/core.css';
+import {IonApp, IonButton, IonDatetime, IonRouterOutlet} from '@ionic/react';
+import {IonReactRouter} from "@ionic/react-router";
+import Home from "./components/Home";
 function App() {
+  /*  const [courseGoals,setCourseGoals]=useState(
+        [{id:1,text:'Finish the Course'},
+        {id:2,text:'Learn all about the Course Main Topic'},
+        {id:3,text:'Help other students in the Course Q&amp;A'}])
+const addGoalsHandler=(newGoal)=>{
+// setCourseGoals(courseGoals.concat(newGoal));
+    setCourseGoals(prev=>prev.concat(newGoal))
+console.log(courseGoals);*/
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ /*   <div title="this works" className="Att">
+     hellgho world
+        <NewGoal  onAddGoal={addGoalsHandler}/>
+   <GoalList  goals={courseGoals} />
+    </div>*/
+     <div>
+         <IonApp>
+             <IonReactRouter>
+                 <IonRouterOutlet>
+                 </IonRouterOutlet>
+             </IonReactRouter>
+         </IonApp>
+<Home/>
+     </div>
   );
 }
 
